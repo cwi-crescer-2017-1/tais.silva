a0ac0c04b53130dd90bc7d87846eff88b7f40c25
Tais <tais.silva@cwi.com.br>
2017-04-18 17:40:40 -0300

    feat(Golpes)
    Acrescentar ArrayList.

--- oscavaleirosdozodiaco/Constelacao.java
+++ oscavaleirosdozodiaco/Constelacao.java
@@ -1,22 +1,20 @@
+import java.util.ArrayList;
+
 public class Constelacao {
     private String nome;
-    private Golpe[] golpes = new Golpe[3];
+    //private Golpe[] golpes = new Golpe[3];
+	private ArrayList golpes = new ArrayList();
     private int ultimaPosicaoPreenchida = 0;
 
     public Constelacao(String nome) {
         this.nome = nome;
-    }
-    
-    public String getNome() {
-        return this.nome;
-    }
-    
-    public Golpe[] getGolpes() {
-        return this.golpes;
-    }
+    }    
     
     public void adicionarGolpe(Golpe golpe) {
         golpes[ultimaPosicaoPreenchida++] = golpe;        
     }
     
+	public Golpe[] getGolpes() {
+        return this.golpes;
+    }
 }
