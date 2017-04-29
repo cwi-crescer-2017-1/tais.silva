0afc01b67497703f3c7aae5af3c549b05f9ac269
Tais <tais.silva@cwi.com.br>
2017-04-29 14:12:31 -0300

    feat(ataqueduplo) acrescentar classe ataque duplo

---
+++ oscavaleirosdozodiaco/AtaqueDuplo.java
@@ -0,0 +1,12 @@
+public class AtaqueDuplo implements Movimento
+{
+    private Saint golpeador, golpeado;
+    public void AtaqueDuplo(Saint golpeador, Saint golpeado){
+        this.golpeador = golpeador;
+        this.golpeado = golpeado;
+    }
+    
+    public void executar() {
+        this.golpeador
+    }
+}
