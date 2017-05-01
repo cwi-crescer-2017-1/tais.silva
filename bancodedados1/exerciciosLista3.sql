99b85016b0bac656d017fe5370bceb374cd4e2ef
Taís Jaques <tais.silva@cwi.com.br>
2017-05-01 11:24:51 -0300

    feat(ExerciciosLista3)
    Acrescentar exercícios da Lista 3 de Banco de Dados. Sobre Empregados, Departamento e Cidade/Estado.

---
+++ bancodedados1/exerciciosLista3.sql
@@ -0,0 +1,268 @@
+
+create table Empregado (
+ IDEmpregado    int not null
+,NomeEmpregado  varchar(30) not null
+,Cargo          varchar(15) not null
+,IDGerente      int null
+,DataAdmissao   datetime not null
+,Salario        decimal(7,2) not null
+,Comissao       decimal(7,2)
+,IDDepartamento int
+);
+
+insert into Empregado values (7369 ,'SMITH', 'Atendente', '7902', convert(datetime, '1980/12/17', 111), 800, null, 20);
+insert into Empregado values (7499 ,'ALLEN', 'Vendedor', '7698', convert(datetime, '1981/02/20', 111), 1600, 300, 30);
+insert into Empregado values (7521 ,'WARD', 'Vendedor', '7698', convert(datetime, '1981/02/22', 111), 1250, 500, 30);
+insert into Empregado values (7566 ,'JONES', 'Gerente', '7839', convert(datetime, '1981/04/02', 111), 2975, null, 20);
+insert into Empregado values (7654 ,'MARTIN', 'Vendedor', '7698', convert(datetime, '1981/09/28', 111), 1250, 1400, 30);
+insert into Empregado values (7698 ,'BLAKE', 'Gerente', '7839', convert(datetime, '1981/05/01', 111), 2850, null, 30);
+insert into Empregado values (7782 ,'CLARK', 'Gerente', '7839', convert(datetime, '1981/06/09', 111), 2450, null, 10);
+insert into Empregado values (7788 ,'SCOTT', 'Analista', '7566', convert(datetime, '1982/12/09', 111), 3000, null, 20);
+insert into Empregado values (7839 ,'KING', 'Presidente', null, convert(datetime, '1981/11/17', 111), 5000, null, null);
+insert into Empregado values (7844 ,'TURNER', 'Vendedor', '7698', convert(datetime, '1981/09/08', 111), 1500, 0, 30);
+insert into Empregado values (7876 ,'ADAMS', 'Atendente', '7788', convert(datetime, '1983/01/12', 111), 1100, null, 20);
+insert into Empregado values (7900 ,'JAMES', 'Atendente', '7698', convert(datetime, '1981/12/03', 111), 950, null, 30);
+insert into Empregado values (7902 ,'FORD', 'Analista', '7566', convert(datetime, '1981/12/03', 111), 3000, null, 20);
+insert into Empregado values (7934 ,'MILLER', 'Atendente', '7782', convert(datetime, '1982/01/23', 111), 1300, null, 10);
+insert into Empregado values (7940 ,'ANDREW', 'Atendente', '7782', convert(datetime, '1988/01/20', 111), 1150, null, null);
+
+create table Departamento (
+ IDDepartamento   int not null
+,NomeDepartamento varchar(30)
+,Localizacao      varchar(25)
+);
+
+insert into Departamento values (10,'Contabilidade', 'SAO PAULO');
+insert into Departamento values (20,'Pesquisa', 'SAO LEOPOLDO');
+insert into Departamento values (30,'Vendas', 'SAO PAULO');
+insert into Departamento values (40,'Operações', 'RECIFE');
+insert into Departamento values (1, 'Presidência', 'RIBEIRAO PRETO');
+insert into Departamento values (60, 'Tecnologia', 'PORTO ALEGRE');
+
+---------
+ALTER TABLE Departamento ADD  CONSTRAINT PK_departamento 
+  PRIMARY KEY  
+  (IDDepartamento);
+
+ALTER TABLE Empregado ADD  CONSTRAINT PK_Empregado
+  PRIMARY KEY  
+  (IDEmpregado);
+
+ALTER TABLE Empregado ADD CONSTRAINT FK_Empregado_Departamento
+  FOREIGN KEY (IDDepartamento) REFERENCES Departamento (IDDepartamento);
+
+CREATE INDEX IX_Empregado_Departamento ON Empregado (IDDepartamento);
+
+alter table empregado add DataNascimento date;
+
+begin transaction
+go
+
+update Empregado set DataNascimento = convert(date, '22/12/1962', 103) where idEmpregado = 	7369;
+update Empregado set DataNascimento = convert(date, '25/02/1961', 103) where idEmpregado = 	7499;
+update Empregado set DataNascimento = convert(date, '27/02/1962', 103) where idEmpregado = 	7521;
+update Empregado set DataNascimento = convert(date, '09/04/1952', 103) where idEmpregado = 	7566;
+update Empregado set DataNascimento = convert(date, '06/10/1950', 103) where idEmpregado = 	7654;
+update Empregado set DataNascimento = convert(date, '07/05/1959', 103) where idEmpregado = 	7698;
+update Empregado set DataNascimento = convert(date, '15/06/1956', 103) where idEmpregado = 	7782;
+update Empregado set DataNascimento = convert(date, '13/12/1966', 103) where idEmpregado = 	7788;
+update Empregado set DataNascimento = convert(date, '22/11/1961', 103) where idEmpregado = 	7839;
+update Empregado set DataNascimento = convert(date, '15/09/1954', 103) where idEmpregado = 	7844;
+update Empregado set DataNascimento = convert(date, '22/01/1944', 103) where idEmpregado = 	7876;
+update Empregado set DataNascimento = convert(date, '10/12/1955', 103) where idEmpregado = 	7900;
+update Empregado set DataNascimento = convert(date, '09/12/1957', 103) where idEmpregado = 	7902;
+update Empregado set DataNascimento = convert(date, '30/01/1955', 103) where idEmpregado = 	7934;
+update Empregado set DataNascimento = convert(date, '24/01/1969', 103) where idEmpregado = 	7940;
+
+COMMIT 
+go
+
+-- Exercício 1: Datas 
+BEGIN TRANSACTION 
+
+Select IDEmpregado			 as ID, 
+	   NomeEmpregado		 as Nome,
+	   DATEDIFF(year, DataNascimento, DataAdmissao)  as Idade
+From Empregado
+Where DataAdmissao BETWEEN convert(date, '01/01/1980', 103) and convert(date, '31/12/1980', 103); 
+
+COMMIT 
+go
+
+-- Exercício 2: Ranking
+BEGIN TRANSACTION
+
+Select TOP(1) Cargo,
+	   COUNT(Cargo) as Total
+From   Empregado
+Group By Cargo
+Order by Total DESC;
+
+COMMIT
+go
+
+-- Exercício 3: Contagem 
+BEGIN TRANSACTION
+
+Create table Cidade 
+(
+  IDCidade  int         not null,
+  Nome      varchar(30) not null,
+  UF        varchar(2)  not null,
+    constraint PK_Cidade primary key (IDCidade)
+);	
+
+Create table Associado 
+(
+  IDAssociado    int         not null,
+  Nome           varchar(50) not null,
+  DataNascimento datetime    not null,
+  Sexo           char(1)     not null,
+  CPF            varchar(11) ,
+  Endereco       varchar(35),
+  Bairro         varchar(25),
+  Complemento    varchar(20),
+  IDCidade       int,
+     constraint PK_Associado primary key (IDAssociado),
+	 constraint FK_Associado foreign key (IDCidade)
+	    references Cidade (IDCidade)
+);
+
+insert into Cidade (IDCidade, Nome, UF)
+   values (1, 'Sao Leopoldo', 'RS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (2, 'Porto Alegre', 'RS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (3, 'Parobe', 'RS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (4, 'Taquara', 'RS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (105, 'São Paulo', 'SP');
+insert into Cidade (IDCidade, Nome, UF)
+   values (106, 'Guarulhos', 'SP');
+insert into Cidade (IDCidade, Nome, UF)
+   values (27, 'Rio de Janeiro', 'RJ');
+insert into Cidade (IDCidade, Nome, UF)
+   values (108, 'Brasilia', 'DF');
+insert into Cidade (IDCidade, Nome, UF)
+   values (109, 'Belo Horizonte', 'MG');
+insert into Cidade (IDCidade, Nome, UF)
+   values (110, 'Uberlândia', 'MG');
+insert into Cidade (IDCidade, Nome, UF)
+   values (5, 'Cidade de Alagoas', 'AL');
+insert into Cidade (IDCidade, Nome, UF)
+   values (6, 'Maceio', 'AL');
+insert into Cidade (IDCidade, Nome, UF)
+   values (7, 'Itu', 'SP');
+insert into Cidade (IDCidade, Nome, UF)
+   values (8, 'Campinas', 'SP');
+insert into Cidade (IDCidade, Nome, UF)
+   values (9, 'Guaratingueta', 'SP');
+insert into Cidade (IDCidade, Nome, UF)
+   values (10, 'Santa Barbara', 'MG');
+insert into Cidade (IDCidade, Nome, UF)
+   values (11, 'Campos dos Goitacases', 'RJ');
+insert into Cidade (IDCidade, Nome, UF)
+   values (12, 'Sao Gabriel', 'RS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (13, 'Brasopolis', 'MG');
+insert into Cidade (IDCidade, Nome, UF)
+   values (14, 'Cristina', 'MG');
+insert into Cidade (IDCidade, Nome, UF)
+   values (15, 'Umbuzeiro', 'PB');
+insert into Cidade (IDCidade, Nome, UF)
+   values (16, 'Vicosa', 'MG');
+insert into Cidade (IDCidade, Nome, UF)
+   values (17, 'Macae', 'RJ');
+insert into Cidade (IDCidade, Nome, UF)
+   values (18, 'Sao Luis', 'MA');
+insert into Cidade (IDCidade, Nome, UF)
+   values (19, 'Cuiaba', 'MT');
+insert into Cidade (IDCidade, Nome, UF)
+   values (20, 'Sao Borja', 'RS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (21, 'Natal', 'RN');
+insert into Cidade (IDCidade, Nome, UF)
+   values (22, 'Diamantina', 'MG');
+insert into Cidade (IDCidade, Nome, UF)
+   values (23, 'Campo Grande', 'MS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (24, 'Fortaleza', 'CE');
+insert into Cidade (IDCidade, Nome, UF)
+   values (25, 'Bage', 'RS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (26, 'Bento Goncalves', 'RS');
+insert into Cidade (IDCidade, Nome, UF)
+   values (28, 'Pinheiro', 'MA');
+insert into Cidade (IDCidade, Nome, UF)
+   values (29, 'Salvador', 'BA');
+insert into Cidade (IDCidade, Nome, UF)
+   values (30, 'Garanhuns', 'PE');
+Insert into Cidade (IDCidade, Nome, UF)
+   values (31, 'São Paulo', 'SP');    
+Insert into Cidade (IDCidade, Nome, UF)
+   values (32, 'Guarulhos', 'SP');    
+Insert into Cidade (IDCidade, Nome, UF)
+   values (33, 'Rio de Janeiro', 'RJ');    
+Insert into Cidade (IDCidade, Nome, UF)
+   values (34, 'Brasilia', 'DF');    
+Insert into Cidade (IDCidade, Nome, UF)
+   values (35, 'Belo Horizonte', 'MG');        
+Insert into Cidade (IDCidade, Nome, UF)
+   values  (36, 'Uberlândia', 'MG');      
+Insert into Cidade (IDCidade, Nome, UF)
+   values  (37, 'Santana do Livramento', 'RS');
+Insert into Cidade (IDCidade, Nome, UF)
+   values  (38, 'Santa Maria', 'RS');
+Insert into Cidade (IDCidade, Nome, UF)
+   values  (39, 'Santa Rosa', 'RS');
+Insert into Cidade (IDCidade, Nome, UF)
+   values  (40, 'São Francisco de Paula', 'RS');    
+
+-- ASSOCIADOS...
+insert into Associado 
+      (IDAssociado, 
+       Nome, 
+       DataNascimento, 
+       Sexo)
+  values 
+      (1, 
+       'Ana Paula Padrao', 
+       convert(datetime, '10/05/1980', 103), 
+       'F' );
+--
+insert into Associado 
+      (IDAssociado, 
+       Nome, 
+       DataNascimento, 
+       Sexo)
+  values  
+      (2, 
+       'Joao Soares', 
+       convert(datetime, '20/02/1980', 103), 
+       'M' );
+
+select UF as Estado,
+	  COUNT(Nome) as 'Total de Cidades'	   
+From Cidade
+Group By UF;
+
+COMMIT
+go
+
+-- Exercícios 4: Alterando dados
+BEGIN TRANSACTION 
+
+-- Inserir departamento Inovação
+Insert into Departamento
+	(IDDepartamento, NomeDepartamento, Localizacao)
+Values
+	(70, 'Inovação', 'SAO LEOPOLDO');
+
+-- Atualizar departamento de quem foi adimitido em Dezembro para Inovação
+Update Empregado
+Set    IDDepartamento = 70
+Where DATEPART(Month, DataAdmissao) = 12 and Cargo != 'Atendente'
+
+COMMIT
+go
