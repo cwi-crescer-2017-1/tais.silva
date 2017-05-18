baace21db0fd5cb46b665c02f33877dd45fa291c
Tais <tais.silva@cwi.com.br>
2017-05-18 00:20:12 -0300

    feat(AngularJS)
    Acrescentar primeiro tema de Angular. Busca com filter.

--- AngularJS/Dia1/js/controllers.js
+++ AngularJS/Dia1/js/controllers.js
@@ -1,19 +1,40 @@
-var modulo1 = angular.module('modulo1', []);
+var dia1 = angular.module('dia1', []);
 
-modulo1.controller('formularioPkn', function($scope) {
-  $scope.pokemon = [{
-  nome: '11', 
-  tipo: 'Default' 
-},
-{
-  nome: '22', 
-  tipo: 'Default2' 
-  }]
-});
+dia1.controller('formularioPkn', ['$scope', function(model) {
 
-// modulo1.controller('formularioPkn', ['$scope', function(model) {
-//   model.pokemon = {
-//   nome: 'Joao', 
+	model.pokemon = 
+	[{
+		nome: 'Bulbasaur', 
+		tipo: 'Grass' 
+	},
+	{
+		nome: 'Ivysaur', 
+		tipo: 'Poison' 
+	},
+	{
+		nome: 'Charmander', 
+		tipo: 'Fire' 
+	},
+	{
+		nome: 'Charizard', 
+		tipo: 'Flying' 
+	},
+	{
+		nome: 'Squirtle', 
+		tipo: 'Water' 
+	}]
+
+}]);
+
+
+
+// dia1.controller('formularioPkn', function($scope) {
+//   $scope.pokemon = [{
+//   nome: '11', 
 //   tipo: 'Default' 
-//   }
-// }]);
+// },
+// {
+//   nome: '22', 
+//   tipo: 'Default2' 
+//   }]
+// });
