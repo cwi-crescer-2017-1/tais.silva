b39f493a118cdd3a15e06a36b0147bf1a6ce81a5
Tais <tais.silva@cwi.com.br>
2017-05-18 16:43:20 -0300

    feat(AngularJS)
    Organizar as pastas e relinkar arquivos. Trazer css para arquivo externo.

---
+++ AngularJS/Dia2/Exercicio2/controller.js
@@ -0,0 +1,19 @@
+var dia2 = angular.module('dia2', []);
+
+dia2.controller('mudarData', function ($scope, $filter) {
+
+	$scope.converterData = converterData;
+
+	function converterData() {
+
+	let pattern = '/(\d{2})\/(\d{2})\/(\d{4})/';
+    let replace = '$1.$2.$3';
+    let dataFormatada = $scope.dataDigitada.replace(pattern, replace);
+
+    let dataObjeto = new Date(dataFormatada);
+    
+    $scope.dataObjeto = dataObjeto;
+    // $scope.dataPronta = $filter('date')(dataObjeto, 'mediumDate')
+  }
+
+});
