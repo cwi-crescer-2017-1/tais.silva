d72a4901ef53e11c0a7c398566c169d54d47ea03
Tais <tais.silva@cwi.com.br>
2017-05-18 16:34:21 -0300

    feat(Exercicio1-data)
    Acrescentar pasta para o exercício.

--- AngularJS/Dia2/Exercício1-data/controller.js
+++ AngularJS/Dia2/Exercício1-data/controller.js
@@ -9,7 +9,6 @@ dia2.controller('mudarData', function ($scope, $filter) {
 	let pattern = '/(\d{2})\/(\d{2})\/(\d{4})/';
     let replace = '$1.$2.$3';
     let dataFormatada = $scope.dataDigitada.replace(pattern, replace);
-    console.log(dataFormatada);
 
     let dataObjeto = new Date(dataFormatada);
     
