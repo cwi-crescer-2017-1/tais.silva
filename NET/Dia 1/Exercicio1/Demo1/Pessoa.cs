eddac421a14cd30f8c50cb8debfbe6006ee928e3
Tais <tais.silva@cwi.com.br>
2017-05-24 17:36:11 -0300

    feat(NET)
    Acrescentar pasta NET.

---
+++ NET/Dia 1/Exercicio1/Demo1/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    public class Pessoa
+    {
+        public const double PI = 3.14;
+        public readonly double PIreadonly = 3.14;
+
+        /*internal 
+        private 
+        protected 
+        readonly*/
+
+        public string Nome { get; set; }
+        public int? Id { get; set; }
+
+        public DateTime Nascimento { get; set; }
+    }
+}
---
+++ NET/Dia 1/Exercicio1/Demo1/Programa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            //public const double 
+            //int valorPodeSerNull = null;
+            //Pessoa pessoa = new Pessoa();
+            //pessoa.Id = null;
+            //pessoa.Nascimento = new DateTime(1982, 10, 29);
+            //pessoa.Nome = $"Giovani Decusati {pessoa.Id}";
+            ////if (pessoa.Id.HasValue)
+            //Console.WriteLine(pessoa.Nome);
+            bool continua = true;
+            var entradas = new int[] { };
+            Console.WriteLine("Digite um valor:");
+
+            while (continua)
+            {
+                var entrada = Console.ReadLine();
+                if (entrada == "exist")
+                {
+                    break;
+                }
+
+                var nEntradas = entradas.Length;
+
+                var novaEntrada = new int[nEntradas.Length + 1];                
+
+                for (int i = 0; i < novaEntrada.Length; i++)
+                {
+                    novaEntrada[i] = entradas[i];
+                }
+
+                novaEntrada[nEntradas] = int.Parse(entrada);
+
+                entradas = novaEntrada;
+            }
+            Console.ReadKey();
+        }
+    }
+}
