341189749e886a860ebcaca183d619ea1d142e65
Tais <tais.silva@cwi.com.br>
2017-05-27 17:52:32 -0300

    feat(Chat)
    Acrescentar criação de API do chat inicial.

---
+++ NET/chat/chat/Models/Mensagem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chat.Models
+{
+    public class Mensagem
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+        public Usuario Usuario { get; private set; }
+        public DateTime DateTime { get; private set; }
+
+        public Mensagem(string texto, Usuario usuario)
+        {
+            this.Texto = texto;
+            this.Usuario = usuario;
+            this.DateTime = DateTime.Now;
+        }
+    }
+}
---
+++ NET/chat/chat/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chat.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string UrlImagemPerfil { get; set; }
+
+        public Usuario(string nome, string url)
+        {
+            this.Nome = Nome;
+            this.UrlImagemPerfil = url;
+        }
+    }
+}
