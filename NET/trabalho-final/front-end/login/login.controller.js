eb5b67094e5b8263a1139af36b6fa0bf45255f90
Tais <tais.silva@cwi.com.br>
2017-06-09 00:57:47 -0300

    feat(Front-end) Acrescentar auth service direto.

--- NET/trabalho-final/front-end/login/login.controller.js
+++ NET/trabalho-final/front-end/login/login.controller.js
@@ -1,6 +1,6 @@
 angular
 	.module('app')
-	.controller('LoginController', function ($rootScope, $scope, authService, $location) {
+	.controller('LoginController', function ($rootScope, $scope, authService, $location, $localStorage, toastr) {
 		$scope.controller = 'LoginController';
 		$rootScope.logado = authService.isAutenticado();
 
@@ -9,18 +9,24 @@ angular
 	    authService.login(usuario)
 	      .then(
 	        function (response) {
-	          console.log(response);
-	          alert('Login com sucesso!');
-	          $rootScope.logado = true;
+	          toastr.success('Login realizado com sucesso.', 'Você está dentro!');
+	          var usuarioLogado = response.data.dados;
+
+				if(!angular.isArray(usuarioLogado.Permissoes)) {
+				    usuarioLogado.Permissoes = [usuarioLogado.Permissoes];
+				}
+
+				$localStorage.usuarioLogado = usuarioLogado;
+	          	$rootScope.logado = true;
 	        },
 	        function (response) {
-	          console.log(response);
-	          alert('Erro no Login!');
+	          toastr.warning('Erro no login.', 'Depois tente novamente!');
 	        });
 	  };
 
 	  $scope.logout = function (usuario) {
 	    authService.logout(usuario);
 	    $rootScope.logado = false;
+	    toastr.success('Logout realizado com sucesso.', 'Você está fora!');
 	};
 	});
