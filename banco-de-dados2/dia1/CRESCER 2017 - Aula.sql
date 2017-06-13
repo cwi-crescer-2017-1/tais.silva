08136b2cea82cb8c4191c019c0304c61ab72d1aa
Tais <tais.silva@cwi.com.br>
2017-06-13 20:03:02 -0300

    feat(banco-de-dados2) Acrescentar lista de exercícios 2.

--- banco-de-dados2/dia1/CRESCER 2017 - Aula.sql
+++ banco-de-dados2/dia1/CRESCER 2017 - Aula.sql
@@ -7,11 +7,12 @@ CREATE TABLE Pessoa (
 Create sequence SQPessoa;
 
 Insert Into Pessoa (IDPessoa, Nome)
- Values (SQPessoa.nextval, 'Andre');
+ Values (SQPessoa.nextval, 'Andre'); 
  
  COMMIT;
 
 -- Produtos inativos
+-- Liste os produtos (id e nome) que não tiveram nenhuma compra nos últimos quatro meses.
 select IDPRODUTO, NOME
 from PRODUTO 
 where IDPRODUTO NOT IN
@@ -19,35 +20,44 @@ where IDPRODUTO NOT IN
 from PEDIDOITEM pi
 Inner Join PEDIDO pd
           on pi.IDPEDIDO = pd.IDPEDIDO 
-          where DATAPEDIDO >= ADD_MONTHS(SYSDATE,-4))
+          where DATAPEDIDO >= ADD_MONTHS(SYSDATE,-4));
+
+-- Abaixo, solução do André:          
+SELECT IDProduto, Nome, Situacao 
+FROM Produto
+WHERE IDProduto NOT IN  (SELECT IDProduto 
+                         FROM PedidoItem item
+                         INNER JOIN Pedido ped ON item.IDPedido = ped.IDPedido
+                         WHERE ped.DataPedido >= ADD_MONTHS(TRUNC(SYSDATE),-4));
+
 
 -- Alterando status
 -- Altere o status dos produtos (campo situacao) que não tiveram nenhum pedido nos últimos quatro meses.
 update PRODUTO 
 set SITUACAO = 'I'
-where IDPRODUTO NOT IN
-(select IDPRODUTO 
-from PEDIDOITEM pi
-Inner Join PEDIDO pd
-          on pi.IDPEDIDO = pd.IDPEDIDO 
-          where DATAPEDIDO >= ADD_MONTHS(SYSDATE,-4))
-          
+WHERE IDProduto NOT IN  (SELECT IDProduto 
+                         FROM PedidoItem item
+                         INNER JOIN Pedido ped ON item.IDPedido = ped.IDPedido
+                         WHERE ped.DataPedido >= ADD_MONTHS(TRUNC(SYSDATE),-4));
+
+COMMIT;
 -- Parametro
 -- Faça uma consulta que receba um parâmetro sendo o IDProduto e liste a quantidade 
 -- de itens na tabela PedidoItem com este IDProduto foram vendidos
--- no último ano (desde janeiro/2017).
-DEFINE IdParametro = 5          
+-- no último ano (desde janeiro/2017).  
 select pr.IDPRODUTO, COUNT(pi.QUANTIDADE) 
 from PRODUTO pr
 join PEDIDOITEM pi
 on pr.IDPRODUTO = pi.IDPRODUTO
-and pi.IDPRODUTO = :IdParametro
+and pi.IDPRODUTO = :IdProduto
 join PEDIDO pd 
 on pi.IDPEDIDO = pd.IDPEDIDO
 where pd.DATAPEDIDO >= TO_DATE(201701, 'YYYYMM')
-group by pr.IDPRODUTO 
+group by pr.IDPRODUTO; 
 
+select * from user_tables;
 
+EXEC dbms_stats.gather_schema_stats( USER);
 
 
       
---
+++ banco-de-dados2/dia1/CRESCER 2017 - lista2.sql
@@ -0,0 +1,89 @@
+-- Exercício 1
+-- Atualmente a tabela de Cidade tem registros duplicados (nome e UF).
+-- Faça um código (PL/SQL) que liste quais são as cidades duplicadas. 
+-- Após isso liste todos os clientes que estão relacionados com estas cidades
+DECLARE
+   CURSOR C_ListaDupli IS
+     Select Nome,
+            Uf
+     From   Cidade
+     Group  By Nome, Uf
+     Having count(1) > 1;
+   --
+   CURSOR C_ListaCli IS
+     Select cl.IDCliente,
+            cl.Nome as NomeCliente,
+            cd.Nome,
+            cd.Uf
+     From   Cidade cd
+     Inner Join Cliente cl on cl.IDCidade = cd.IDCidade
+     Order By cl.Nome;
+BEGIN
+  FOR cli IN C_ListaCli LOOP
+    FOR dup IN C_ListaDupli LOOP
+      if(cli.Nome = dup.Nome and cli.Uf = dup.Uf) then 
+        DBMS_OUTPUT.PUT_LINE('Nome: ' || cli.NomeCliente);
+      END IF;
+    END LOOP;
+   END LOOP;
+END;
+
+-- Exercício 2
+-- Faça uma rotina que permita atualizar o valor do pedido a partir dos seus itens.
+--Esta rotina deve receber por parametro o IDPedido e então verificar o valor total de seus itens (quantidade x valor unitário).
+DECLARE
+ CURSOR C_ListaPed (pIDPedido in number) IS
+     Select SUM(Quantidade * PrecoUnitario) as ValorTotal
+     From   PedidoItem
+     Where  IDPedido = pIDPedido;
+  vPedido  Pedido.IDPedido%TYPE;
+BEGIN
+   vPedido := 5;
+   for reg in C_ListaPed(vPedido)loop
+      update PEDIDO
+      set VALORPEDIDO = reg.ValorTotal
+      where IDPedido = vPedido;
+    end loop;   
+END;
+
+-- Exercício 3
+-- Crie uma rotina que atualize todos os clientes que não realizaram nenhum 
+-- pedido nos últimos 6 meses (considere apenas o mês, dia 01 do 6º mês anterior). 
+-- Definir o atributo Situacao para I.
+DECLARE
+  CURSOR C_ListaCli IS
+    SELECT c.IDCliente, c.Nome, c.Situacao 
+    FROM Cliente c
+    INNER JOIN Pedido p ON c.IDCliente = p.IDCliente
+    WHERE IDPedido NOT IN  (SELECT IDPedido 
+                             FROM PedidoItem item
+                             WHERE p.DataPedido >= ADD_MONTHS(TRUNC(SYSDATE),-6));
+BEGIN
+   for reg in C_ListaCli loop
+      update CLIENTE
+      set SITUACAO = 'I'
+      where IDCliente = reg.IDCliente;
+    end loop;   
+END;
+
+-- Exercício 4
+-- Faça uma rotina que receba dois parâmetros: IDProduto & Mês e Ano
+-- E então faça uma rotina que verifique no ANO/MÊS para o produto informado qual
+-- a lista de materiais (incluindo a quantidade) é necessário para atender todos 
+-- os Pedidos desde período. Deve imprimir o nome do material e quantidade total.
+DECLARE
+ CURSOR C_ListaPed (pIDPedido in number) IS
+     Select SUM(Quantidade * PrecoUnitario) as ValorTotal
+     From   PedidoItem
+     Where  IDPedido = pIDPedido;
+  vProduto  Produto.IDProduto%TYPE;
+  vData     Pedido.DataEntrega%TYPE;
+BEGIN
+   vProduto := 5;
+   vData := 00;
+   for reg in C_ListaPed(vPedido)loop
+      update PEDIDO
+      set VALORPEDIDO = reg.ValorTotal
+      where IDPedido = vPedido;
+    end loop;   
+END;
