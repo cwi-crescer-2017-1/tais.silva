90b9d0459adb28b9651ec8c50cf328fb83d54290
Tais <tais.silva@cwi.com.br>
2017-06-21 13:28:16 -0300

    feat(tema1) Acrescentar último exercício de parcelamento.

--- java/tema1/src/main/java/br/com/crescer/tema1/ParceladorGeral.java
+++ java/tema1/src/main/java/br/com/crescer/tema1/ParceladorGeral.java
@@ -6,7 +6,9 @@
 package br.com.crescer.tema1;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -14,18 +16,26 @@ import java.util.Map;
  * @author Taís
  */
 
-//public class ParceladorGeral implements Parcelador {
-//    /*O método calcular deve retornar um map com a data de vencimento (dd/MM/yyyy) da parcela e o valor (R$ .....).
-//    // Ex. valor: 1000  juros: 10%, parcelas: 10  data: 30/06/2016
-//    1. 30/06/2016 - R$ 110,00
-//    2. 31/07/2016 - R$ 110,00
-//    3. 30/08/2016 - R$ 110,00 ...
-//    */
-//    @Override
-//    public Map<String, BigDecimal> calcular(BigDecimal valorParcelar, int numeroParcelas, double taxaJuros, Date dataPrimeiroVencimento){
-//        
-//        
-//        return 
-//    };
-//    
-//}
+public class ParceladorGeral implements Parcelador {
+    /*O método calcular deve retornar um map com a data de vencimento (dd/MM/yyyy) da parcela e o valor (R$ .....).
+    // Ex. valor: 1000  juros: 10%, parcelas: 10  data: 30/06/2016
+    1. 30/06/2016 - R$ 110,00
+    2. 31/07/2016 - R$ 110,00
+    3. 30/08/2016 - R$ 110,00 ...
+    */
+    @Override
+    public Map<String, BigDecimal> calcular(BigDecimal valorParcelar, int numeroParcelas, double taxaJuros, Date dataPrimeiroVencimento){
+        Map map = new HashMap();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dataPrimeiroVencimento);
+        Double parcelaComJuros = (valorParcelar.doubleValue() * (taxaJuros / 100)) / numeroParcelas;
+        
+        int i = 0;
+        while( i < numeroParcelas) {
+            i++;            
+            map.put(parcelaComJuros, calendar.getTime());
+            calendar.add(Calendar.MONTH, 1);
+        }
+        return map;
+    };   
+}
